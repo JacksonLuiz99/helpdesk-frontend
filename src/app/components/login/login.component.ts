a3795e43e9275e272ce785a2ab93ad07c618a700
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2024-12-26 22:42:41 -0400

    requisição HTTP incompleta

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,14 +19,16 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService) { }
 
   ngOnInit(): void { }
 
   logar() {
-    this.toast.error('Dados de login inválidos, corrija e tente novamente!', 'Login');
-    this.creds.senha = '';
-
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.toast.info(resposta.headers.get('Authorization'))
+    })
   }
 
   validaCampos(): boolean {
